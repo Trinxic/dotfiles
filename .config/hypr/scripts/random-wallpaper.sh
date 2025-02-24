90bd67aafc5ac9405f8b3e520b9b47228598e429
Trinxic <davida_2016@outlook.com>
2025-02-24 04:30:24 -0500

    Edits to hyprpaper script [can ignore this lol]. Added `swww` config (which really is just the actual wallpaper script). Used `setup.sh` so `.stow-local-ignore` is set for linux currently.

--- .config/hypr/scripts/random-wallpaper.sh
+++ .config/hypr/scripts/random-wallpaper.sh
@@ -2,14 +2,16 @@
 
 hyprctl hyprpaper unload all
 
+WALLPAPER_DIR="$HOME/dotfiles/stownt/photos/wallpapers"
+
 # get number of wallpapers
-max=$(ls $HOME/dotfiles/photos/wallpapers/ | wc -l)
+max=$(ls WALLPAPER_DIR | wc -l)
 min=1 # first wallpaper ends in '-1'
 
 if [ $max -gt 0 ]; then
     # assumes all wallpapers are consecutively named `wallpaper-<n>.jpg``
     n=$((RANDOM % ($max - $min + 1) + $min))
-    hyprctl hyprpaper preload "$HOME/Wallpapers/wallpaper-$n.jpg"
-    hyprctl hyprpaper wallpaper "DP-1,$HOME/Wallpapers/wallpaper-$n.jpg"
-    hyprctl hyprpaper wallpaper "DP-2,$HOME/Wallpapers/wallpaper-$n.jpg"
+    hyprctl hyprpaper preload "$WALLPAPER_DIR/wallpaper-$n.jpg"
+    hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER_DIR/wallpaper-$n.jpg"
+    hyprctl hyprpaper wallpaper "DP-2,$WALLPAPER_DIR/wallpaper-$n.jpg"
 fi
---
+++ .config/swww/random.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# For more settings:
+# $ swww img --help
+
+WALLPAPER_DIR="$HOME/dotfiles/stownt/photos/wallpapers"
+
+# -- Get Random Wallpaper -- #
+max=$(ls WALLPAPER_DIR | wc -l)
+min=1
+if [ $max -gt 0 ]; then
+    # assumes all wallpapers are consecutively named
+    # `wallpaper-<n>.jpg`, n ∈ {1, number-of-wallpapers}
+    n=$((RANDOM % ($max - $min + 1) + $min))
+fi
+
+WALLPAPER="$WALLPAPER_DIR/wallpaper-$n.jpg"
+
+# ------ Set Wallpaper(s) ----- #
+swww img $WALLPAPER --outputs=DP-1 --transition-type=random
+swww img $WALLPAPER --outputs=DP-2 --transition-type=random --resize=no
+#                                                          {crop, fit, no}
+
+# use `$ swww img -h`
