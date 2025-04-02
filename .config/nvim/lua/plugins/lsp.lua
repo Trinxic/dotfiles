67425ba44dc9e41d8e58ea6af5565cc960efcca4
Trinxic <davida_2016@outlook.com>
2025-04-02 04:52:20 -0400

    Moved plugins back to just `lua/plugins`. Added Github Copilot to nvim.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,12 @@ return {
     -- Automatically install LSPs and related tools to stdpath for Neovim
     -- Mason must be loaded before its dependents so we need to set it up here.
     -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-    { 'williamboman/mason.nvim', opts = {} },
+    {
+      'williamboman/mason.nvim',
+      opts = {
+        auto_install = true, -- auto install lsp servers when new language is detected.
+      },
+    },
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
 
