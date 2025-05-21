8aa9bc91cd555c1274bd9195325baaffde519612
Trinxic <davida_2016@outlook.com>
2025-05-21 04:09:43 -0400

    Edits to hypr and zsh configs.

--- .config/zsh/configs/exports.zsh
+++ .config/zsh/configs/exports.zsh
@@ -1,7 +1,11 @@
 export BROWSER='zen-browser'
 export EDITOR='nvim'
 export VISUAL='nvim'  # unsure what this one means
+export DOCUMENT="$HOME/Documents"
 export FZF_DEFUALT_COMMAND='ag --hidden --ignore .git -g'  # look more into this one
 # eval "`pip completion --zsh`"
-export PATH="$PATH:/usr/local/go/bin"
 export ALIASES="~/dotfiles/.config/zsh/configs/aliases.zsh"
+
+# PATH
+export PATH="$PATH:/usr/local/go/bin"
+export PATH="$HOME/.local/share/pnpm/bin:$PATH"
